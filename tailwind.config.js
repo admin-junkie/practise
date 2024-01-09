5df09cb44f13d84c2789647b4cd2a218f762a51f
admin-junkie <140873187+admin-junkie@users.noreply.github.com>
2024-01-09 11:55:33 +0545

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+
+        site: "url(./images/ny.png)", 
+      }
+    },
+  },
+  plugins: [],
+}
+
